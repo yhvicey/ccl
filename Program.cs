6f1c5ff8daaf301fe391d56735d70caee31e7d9c
Vicey Wang <vicey@live.com>
2016-08-25 20:37:32 +0800

    Ship v1.0.

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CountCodeLine
+{
+    public class Program
+    {
+        private static string Usage { get; } =
+            "Usage: CountCodeLine <root folder> <suffix> [<suffix>,...] [<options>]\n" +
+            "Count code line in target folder.\n" +
+            "Root folder: Folder to count.\n" +
+            "Suffix: Suffix of code file.Start with '.'.\n" +
+            "Options:\n" +
+            "--ignore-blank, -I    Ignore blank lines.\n" +
+            "--verbose, -V         Show process log\n";
+
+        private static string _rootFolder;
+
+        private static List<string> _suffixs;
+
+        private static bool _ignoreBlank;
+
+        private static bool _verbose;
+
+        private static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+
+            if (!ProcessArguments(args))
+            {
+                Console.WriteLine(Usage);
+                return;
+            }
+
+            VisitDirectory(_rootFolder);
+
+            Console.WriteLine($"Time: {(DateTime.Now - start).TotalMilliseconds:F0} ms.");
+        }
+
+        private static bool ProcessArguments(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                return false;
+            }
+
+            _rootFolder = args[0];
+
+            _suffixs = args.Where(x => x.StartsWith(".")).Select(x => x.Split('.').Last()).ToList();
+
+            _ignoreBlank = args.Any(x => x == "--ignore-blank" || x == "-I");
+
+            _verbose = args.Any(x => x == "--verbose" || x == "-V");
+
+            return true;
+        }
+
+        private static int VisitFile(string path)
+        {
+            try
+            {
+                var count = _ignoreBlank
+                    ? File.ReadAllLines(path).Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x).Count()
+                    : File.ReadAllLines(path).Length;
+
+                if (_verbose && count > 0)
+                {
+                    Console.WriteLine($"{path}: {count} line(s).");
+                }
+
+                return count;
+            }
+            catch (Exception)
+            {
+                if (_verbose)
+                {
+                    Console.WriteLine($"Can't read {path}.");
+                }
+                return 0;
+            }
+        }
+
+        private static int VisitDirectory(string path)
+        {
+            try
+            {
+                var dirs = Directory.GetDirectories(path);
+                var count = dirs.Sum(VisitDirectory);
+
+                var files = Directory.GetFiles(path);
+                count += files.Where(file => _suffixs.Any(x => x == file.Split('.').Last())).Sum(VisitFile);
+
+                if (_verbose && count > 0)
+                {
+                    Console.WriteLine($"{path}: {count} line(s).");
+                }
+                return count;
+            }
+            catch (Exception)
+            {
+                if (_verbose)
+                {
+                    Console.WriteLine($"Can't read {path}.");
+                }
+                return 0;
+            }
+        }
+    }
+}
